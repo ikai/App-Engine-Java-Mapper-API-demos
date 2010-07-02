3d32b81ae2a9276820b13181447476dd1ddede72
Ikai Lan <ikai.lan@gmail.com>
2010-07-02 12:32:50 -0700

    Adding dependencies

--- src/com/ikai/mapperdemo/Mapper.java
+++ src/com/ikai/mapperdemo/Mapper.java
@@ -1,51 +1,58 @@
 package com.ikai.mapperdemo;
 
+import java.util.Date;
 import java.util.logging.Logger;
 
 import org.apache.hadoop.io.NullWritable;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.tools.mapreduce.AppEngineMapper;
 
-public class Mapper extends AppEngineMapper<Key, Entity, NullWritable, NullWritable> {
-	  private static final Logger log = Logger.getLogger(Mapper.class.getName());
+public class Mapper extends
+		AppEngineMapper<Key, Entity, NullWritable, NullWritable> {
+	private static final Logger log = Logger.getLogger(Mapper.class.getName());
 
-	  public Mapper() {
-	  }
+	private DatastoreService datastore;
 
-	  @Override
-	  public void taskSetup(Context context) {
-	    log.warning("Doing per-task setup");
-	  }
+	public Mapper() {
+	}
 
-	  @Override
-	  public void taskCleanup(Context context) {
-	    log.warning("Doing per-task cleanup");
-	  }
+	@Override
+	public void taskSetup(Context context) {
 
-	  @Override
-	  public void setup(Context context) {
-	    log.warning("Doing per-worker setup");
-	  }
+	}
 
-	  @Override
-	  public void cleanup(Context context) {
-	    log.warning("Doing per-worker cleanup");
-	  }
+	@Override
+	public void taskCleanup(Context context) {
+		log.warning("Doing per-task cleanup");
+	}
 
-	  // This is a silly mapper that's intended to show some of the capabilities of the API.
-	  @Override
+	@Override
+	public void setup(Context context) {
+		this.datastore = DatastoreServiceFactory.getDatastoreService();
+		log.warning("Doing per-worker setup");
+	}
+
+	@Override
+	public void cleanup(Context context) {
+		log.warning("Doing per-worker cleanup");
+	}
+
+	@Override
 	  public void map(Key key, Entity value, Context context) {
 	    log.warning("Mapping key: " + key);
-	    if (value.hasProperty("skub")) {
-	      // Counts the number of jibbit and non-jibbit skub.
-	      // These counts are aggregated and can be seen on the status page.
-	      if (value.getProperty("skub").equals("Pro")) {
-	        context.getCounter("Skub", "Pro").increment(1);
-	      } else {
-	        context.getCounter("Skub", "Anti").increment(1);
-	      }
+	    	    
+	    if (value.hasProperty("comment")) {
+	    	String comment = (String) value.getProperty("comment");
+	    	comment = comment.toLowerCase();
+	    	value.setProperty("comment", comment);
+	    	value.setProperty("updatedAt", new Date());
+	    	
+	    	datastore.put(value);
+	    	
 	    }
 	  }
-	}
+}
